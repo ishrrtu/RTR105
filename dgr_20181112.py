1adc1ab44ea21a1bcfebf7f031d2996edd599aa8
user <igorj810@gmail.com>
2018-11-12 13:55:55 +0200

    20181112_13_55

---
+++ dgr_20181112.py
@@ -0,0 +1,270 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> from math import *
+>>> vars()
+{'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <class '_frozen_importlib.BuiltinImporter'>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, 'acos': <built-in function acos>, 'acosh': <built-in function acosh>, 'asin': <built-in function asin>, 'asinh': <built-in function asinh>, 'atan': <built-in function atan>, 'atan2': <built-in function atan2>, 'atanh': <built-in function atanh>, 'ceil': <built-in function ceil>, 'copysign': <built-in function copysign>, 'cos': <built-in function cos>, 'cosh': <built-in function cosh>, 'degrees': <built-in function degrees>, 'erf': <built-in function erf>, 'erfc': <built-in function erfc>, 'exp': <built-in function exp>, 'expm1': <built-in function expm1>, 'fabs': <built-in function fabs>, 'factorial': <built-in function factorial>, 'floor': <built-in function floor>, 'fmod': <built-in function fmod>, 'frexp': <built-in function frexp>, 'fsum': <built-in function fsum>, 'gamma': <built-in function gamma>, 'gcd': <built-in function gcd>, 'hypot': <built-in function hypot>, 'isclose': <built-in function isclose>, 'isfinite': <built-in function isfinite>, 'isinf': <built-in function isinf>, 'isnan': <built-in function isnan>, 'ldexp': <built-in function ldexp>, 'lgamma': <built-in function lgamma>, 'log': <built-in function log>, 'log1p': <built-in function log1p>, 'log10': <built-in function log10>, 'log2': <built-in function log2>, 'modf': <built-in function modf>, 'pow': <built-in function pow>, 'radians': <built-in function radians>, 'sin': <built-in function sin>, 'sinh': <built-in function sinh>, 'sqrt': <built-in function sqrt>, 'tan': <built-in function tan>, 'tanh': <built-in function tanh>, 'trunc': <built-in function trunc>, 'pi': 3.141592653589793, 'e': 2.718281828459045, 'tau': 6.283185307179586, 'inf': inf, 'nan': nan}
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+Lietotāj, lūdzu, ievadi argumentu (x): 1.89
+sin(1.89) = 0.95
+a0 = 1.89 S0 = 1.89
+a1 = -1.13 S1 = 0.76
+a2 = 0.20 S2 = 0.97
+a3 = -0.02 S3 = 0.95
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+Lietotāj, lūdzu, ievadi argumentu (x): 5
+sin(5.00) = -0.96
+a0 = 5.00 S0 = 5.00
+a1 = -20.83 S1 = -15.83
+a2 = 26.04 S2 = 10.21
+a3 = -15.50 S3 = -5.29
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+Lietotāj, lūdzu, ievadi argumentu (x): 9
+sin(9.00) = 0.41
+a0 = 9.00 S0 = 9.00
+a1 = -121.50 S1 = -112.50
+a2 = 492.07 S2 = 379.57
+a3 = -949.00 S3 = -569.43
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+versija 1
+Lietotāj, lūdzu, ievadi argumentu (x): 45
+sin(45.00) = 0.85
+a0 = 45.00 S0 = 45.00
+a1 = -15187.50 S1 = -15142.50
+a2 = 1537734.38 S2 = 1522591.88
+a3 = -74140764.51 S3 = -72618172.63
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+versija 2
+Lietotāj, lūdzu, ievadi argumentu (x): 1.89
+sin(1.89) = 0.95
+a0 = 1.89 S0 = 1.89
+a1 = -1.13 S1 = 0.76
+a2 = 0.20 S2 = 0.97
+a3 = -0.02 S3 = 0.95
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+versija 2
+Lietotāj, lūdzu, ievadi argumentu (x): 9
+sin(9.00) = 0.41
+a0 = 9.00 S0 = 9.00
+a1 = -121.50 S1 = -112.50
+a2 = 492.07 S2 = 379.57
+a3 = -949.00 S3 = -569.43
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+versija 2
+Lietotāj, lūdzu, ievadi argumentu (x): 45
+sin(45.00) = 0.85
+a0 = 45.00 S0 = 45.00
+a1 = -15187.50 S1 = -15142.50
+a2 = 1537734.38 S2 = 1522591.88
+a3 = -74140764.51 S3 = -72618172.63
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+Lietotāj, lūdzu, ievadi argumentu (x): 0
+sin(0.00) =   0.00
+a0 =   0.00 S0 =   0.00
+a1 =  -0.00 S1 =   0.00
+a2 =   0.00 S2 =   0.00
+a3 =  -0.00 S3 =   0.00
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+Lietotāj, lūdzu, ievadi argumentu (x): 9
+sin(9.00) =   0.41
+a0 =   9.00 S0 =   9.00
+a1 = -121.50 S1 = -112.50
+a2 = 492.07 S2 = 379.57
+a3 = -949.00 S3 = -569.43
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver2.py ==============
+versija 2
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) = 0.84
+a0 = 1.00 S0 = 1.00
+a1 = -0.17 S1 = 0.83
+a2 = 0.01 S2 = 0.84
+a3 = -0.00 S3 = 0.84
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver2.py ==============
+versija 2
+Lietotāj, lūdzu, ievadi argumentu (x): 2
+sin(2.00) = 0.91
+a0 = 2.00 S0 = 2.00
+a1 = -1.33 S1 = 0.67
+a2 = 0.27 S2 = 0.93
+a3 = -0.03 S3 = 0.91
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver2.py ==============
+versija 2
+Lietotāj, lūdzu, ievadi argumentu (x): 5
+sin(5.00) = -0.96
+a0 = 5.00 S0 = 5.00
+a1 = -20.83 S1 = -15.83
+a2 = 26.04 S2 = 10.21
+a3 = -15.50 S3 = -5.29
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver2.py ==============
+versija 2
+Lietotāj, lūdzu, ievadi argumentu (x): 9
+sin(9.00) = 0.41
+a0 = 9.00 S0 = 9.00
+a1 = -121.50 S1 = -112.50
+a2 = 492.07 S2 = 379.57
+a3 = -949.00 S3 = -569.43
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+versija 1
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) = 0.84
+a0 = 1.00 S0 = 1.00
+a1 = -0.17 S1 = 0.83
+a2 = 0.01 S2 = 0.84
+a3 = -0.00 S3 = 0.84
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+versija 1
+Lietotāj, lūdzu, ievadi argumentu (x): 
+Traceback (most recent call last):
+  File "/home/user/RTR105/teilorarindas_ver1.py", line 3, in <module>
+    x = float(input("Lietotāj, lūdzu, ievadi argumentu (x): "))
+ValueError: could not convert string to float: 
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+versija 1
+Lietotāj, lūdzu, ievadi argumentu (x): 2
+sin(2.00) = 0.91
+a0 = 2.00 S0 = 2.00
+a1 = -1.33 S1 = 0.67
+a2 = 0.27 S2 = 0.93
+a3 = -0.03 S3 = 0.91
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver3.py ==============
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) =   0.84
+a0 =   1.00 S0 =   1.00
+a1 =  -0.17 S1 =   0.83
+a2 =   0.01 S2 =   0.84
+a3 =  -0.00 S3 =   0.84
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver3.py ==============
+versija 3
+Lietotāj, lūdzu, ievadi argumentu (x): 2
+sin(2.00) =   0.91
+a0 =   2.00 S0 =   2.00
+a1 =  -1.33 S1 =   0.67
+a2 =   0.27 S2 =   0.93
+a3 =  -0.03 S3 =   0.91
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver4.py ==============
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) =   0.84
+a0 =   1.00 S0 =   1.00
+a1 =  -0.17 S1 =   0.83
+a2 =   0.01 S2 =   0.84
+a3 =  -0.00 S3 =   0.84
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver4.py ==============
+versija 4
+Lietotāj, lūdzu, ievadi argumentu (x): 2
+sin(2.00) =   0.91
+a0 =   2.00 S0 =   2.00
+a1 =  -1.33 S1 =   0.67
+a2 =   0.27 S2 =   0.93
+a3 =  -0.03 S3 =   0.91
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver5.py ==============
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) =   0.84
+a0 =   1.00 S0 =   1.00
+a1 =  -0.17 S1 =   0.83
+a2 =   0.01 S2 =   0.84
+a3 =  -0.00 S3 =   0.84
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver5.py ==============
+Lietotāj, lūdzu, ievadi argumentu (x): 2
+sin(2.00) =   0.91
+a0 =   2.00 S0 =   2.00
+a1 =  -1.33 S1 =   0.67
+a2 =   0.27 S2 =   0.93
+a3 =  -0.03 S3 =   0.91
+>>> 
+============ RESTART: /home/user/RTR105/teilorarindas_gala_ver.py ============
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+standarta sin(1.00) =   0.84
+Izdruka no liet.f. a0 =   1.00 S0 =   1.00
+Izdruka no liet.f. a1 =  -0.17 S1 =   0.83
+Izdruka no liet.f. a2 =   0.01 S2 =   0.84
+Izdruka no liet.f. a3 =  -0.00 S3 =   0.84
+Izdruka no liet.f. Beigas!
+mans sin(1.00) =   0.84
+>>> 
+============ RESTART: /home/user/RTR105/teilorarindas_gala_ver.py ============
+Lietotāj, lūdzu, ievadi argumentu (x): 2
+standarta sin(2.00) =   0.91
+Izdruka no liet.f. a0 =   2.00 S0 =   2.00
+Izdruka no liet.f. a1 =  -1.33 S1 =   0.67
+Izdruka no liet.f. a2 =   0.27 S2 =   0.93
+Izdruka no liet.f. a3 =  -0.03 S3 =   0.91
+Izdruka no liet.f. Beigas!
+mans sin(2.00) =   0.91
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver1.py ==============
+versija 1
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) = 0.84
+a0 = 1.00 S0 = 1.00
+a1 = -0.17 S1 = 0.83
+a2 = 0.01 S2 = 0.84
+a3 = -0.00 S3 = 0.84
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver2.py ==============
+versija 2
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) = 0.84
+a0 = 1.00 S0 = 1.00
+a1 = -0.17 S1 = 0.83
+a2 = 0.01 S2 = 0.84
+a3 = -0.00 S3 = 0.84
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver3.py ==============
+versija 3
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) =   0.84
+a0 =   1.00 S0 =   1.00
+a1 =  -0.17 S1 =   0.83
+a2 =   0.01 S2 =   0.84
+a3 =  -0.00 S3 =   0.84
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver4.py ==============
+versija 4
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) =   0.84
+a0 =   1.00 S0 =   1.00
+a1 =  -0.17 S1 =   0.83
+a2 =   0.01 S2 =   0.84
+a3 =  -0.00 S3 =   0.84
+>>> 
+============== RESTART: /home/user/RTR105/teilorarindas_ver5.py ==============
+versija 5
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) =   0.84
+a0 =   1.00 S0 =   1.00
+a1 =  -0.17 S1 =   0.83
+a2 =   0.01 S2 =   0.84
+a3 =  -0.00 S3 =   0.84
+>>> 
+============ RESTART: /home/user/RTR105/teilorarindas_gala_ver.py ============
+Lietotāj, lūdzu, ievadi argumentu (x): 1.89
+standarta sin(1.89) =   0.95
+Izdruka no liet.f. a0 =   1.89 S0 =   1.89
+Izdruka no liet.f. a1 =  -1.13 S1 =   0.76
+Izdruka no liet.f. a2 =   0.20 S2 =   0.97
+Izdruka no liet.f. a3 =  -0.02 S3 =   0.95
+Izdruka no liet.f. Beigas!
+mans sin(1.89) =   0.95
+>>> 
