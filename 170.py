0f3255a2c275fcbb4b5d8fb47f65ad68c1de96f4
user <igorj810@gmail.com>
2018-11-26 14:03:10 +0200

    https:/github.com/ishrrtu/RTR105

---
+++ 170.py
@@ -0,0 +1,32 @@
+from math import sin, fabs
+from time import sleep
+
+def f(x):
+    return sin(x)
+
+k = 0
+a = 1.1
+b = 3.2
+
+funa = f(a)
+funb = f(b)
+
+if (funa * funb > 0.0):
+    print("Dotajā intervālā [%s, %s] saknju nav"%(a,b))
+    sleep(1); exit()
+else:
+    print("Dotajā intervālā sakne ir!")
+
+deltax = 0.01
+
+while(fabs(b-a)>deltax):
+    k=k+1
+    x = (a+b)/2; funx = f(x)
+    if (funa*funx<0.):
+        b=x
+    else:
+        a=x
+
+print("Sakne ir: ", x)
+print("Y koordināta ir: ",sin(x))
+print("Iterāciju skaits: ", k)
---
+++ dg17.py
@@ -0,0 +1,101 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1425781250000004
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1425781250000004
+-0.0009854712506998128
+>>> 
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1425781250000004
+Y koordināta ir:  -0.0009854712506998128
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1425781250000004
+Y koordināta ir:  -0.0009854712506998128
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Traceback (most recent call last):
+  File "/home/user/RTR105/170.py", line 23, in <module>
+    k=k+1
+NameError: name 'k' is not defined
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Traceback (most recent call last):
+  File "/home/user/RTR105/170.py", line 25, in <module>
+    k=k+1
+NameError: name 'k' is not defined
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Traceback (most recent call last):
+  File "/home/user/RTR105/170.py", line 22, in <module>
+    x = (a+b)/2; k==k+1; funx = f(x)
+NameError: name 'k' is not defined
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1425781250000004
+Y koordināta ir:  -0.0009854712506998128
+Traceback (most recent call last):
+  File "/home/user/RTR105/170.py", line 30, in <module>
+    print("Iterāciju skaits: ",k)
+NameError: name 'k' is not defined
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1425781250000004
+Y koordināta ir:  -0.0009854712506998128
+Iterāciju skaits: 
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1425781250000004
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1425781250000004
+Y koordināta ir:  -0.0009854712506998128
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Traceback (most recent call last):
+  File "/home/user/RTR105/170.py", line 22, in <module>
+    k=k+1
+NameError: name 'k' is not defined
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Traceback (most recent call last):
+  File "/home/user/RTR105/170.py", line 22, in <module>
+    k=k+1
+NameError: name 'k' is not defined
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1425781250000004
+Y koordināta ir:  -0.0009854712506998128
+Iterāciju skaits:  8
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1415927886962898
+Y koordināta ir:  -1.351064965345797e-07
+Iterāciju skaits:  18
+>>> 
+===================== RESTART: /home/user/RTR105/170.py =====================
+Dotajā intervālā sakne ir!
+Sakne ir:  3.1425781250000004
+Y koordināta ir:  -0.0009854712506998128
+Iterāciju skaits:  8
+>>> 
